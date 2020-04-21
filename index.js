553afe82203da308006a6cfd111d43858910899b
Dominick <dhing@stanford.edu>
2020-04-21 00:15:18 -0400

    Introduced comments, code cleaning

--- index.js
+++ index.js
@@ -2,31 +2,37 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+/*
+ * Define an element handler class for transformation
+ */
 class ElementHandler {
 
   element(element){
+      //Set url to linkedin to open in a new tab
       if (element.tagName === "a"){
           element.setAttribute("href", "https://www.linkedin.com/in/dominickhing/")
           element.setAttribute("target", "_blank")
           element.setInnerContent("linkedin.com/in/dominickhing/")
       }
-
+      //Update paragraph
       if (element.tagName === "p"){
           element.append("Contact me at dhing@stanford.edu. Find me on LinkedIn!")
       }
   }
 
   text(text){
+      //Update in <title> tag and in the body
       if (text.text.trim() === "Variant 1"){
           text.replace("V1 | CloudFlare Fullstack | Dominick Hing");
       } else if (text.text.trim() === "Variant 2") {
           text.replace("V2 | CloudFlare Fullstack | Dominick Hing");
       }
-
   }
-
 }
 
+/*
+ * Define HTML rewriter object
+ */
 const rewriter = new HTMLRewriter()
     .on('title', new ElementHandler())
     .on('h1#title', new ElementHandler())
@@ -38,13 +44,14 @@ const rewriter = new HTMLRewriter()
  * @param {Request} request
  */
 async function handleRequest(request) {
-
     variants = await getVariants().then((data) => {return data;});
-
-    rand = Math.random();
-    if (rand > 0.5){select = variants[0] } else { select = variants[1] }
+    if (Math.random() > 0.5){select = variants[0]} else {select = variants[1]}
     response = await getHTTP(select).then((data) => {return data;});
-    return rewriter.transform(response);
+    if (!response.ok){
+        return rewriter.transform(response);
+    } else {
+        return new Response("The resource could not be obtained. Please try again.");
+    }
 }
 
 /**
